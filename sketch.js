5d158eedbeb201b2571ac6071c598f4c4d94daa1
LOVJOT <lovi.hari@yahoo.com>
2020-10-08 16:12:52 +0530

    ADDED A BOX

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var box10;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -35,7 +36,7 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
-
+    box10 = new Box(200,200,50,50);
 }
 
 function draw(){
@@ -60,5 +61,6 @@ function draw(){
     log5.display();
 
     bird.display();
+    box10.display();
     platform.display();
 }
